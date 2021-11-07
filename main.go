1fea5b792402a3b5e25cefd27fe2894ee2935f36
Mansoor <mansoorchengat@yahoo.com>
2021-11-07 14:14:26 +0530

    Add main

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
 	router.Run()
+}
+
+type Recipe struct {
+	Name         string   `json:"name,omitempty"`
+	Tags         []string `json:"tags,omitempty"`
+	Ingredients  []string
+	Instructions []string
+	PublishedAt  time.Time
 }
